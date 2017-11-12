a14c6ef43b3ccdb0f0bb92a4454684fcd040a716
Damien <bannerotdamien@gmail.com>
2017-11-12 18:02:57 +0100

    create APIManager and APIForecast model
    - create API Manager singleton
    - create call to weather API
    - create APIForecast model
    - add unit tests

--- LBCWeather/Utils.swift
+++ LBCWeather/Utils.swift
@@ -14,3 +14,33 @@ extension String {
 		return NSLocalizedString(self, comment: "")
 	}
 }
+
+
+extension Date {
+	func getForecastDateString() -> String {
+		// set current date in user timezone
+		var forecastDate = Date()
+		var calendar = Calendar.current
+		calendar.timeZone = .current
+		// find hour corresponding to the API hour intervals
+		while calendar.dateComponents([.hour], from: forecastDate).hour!%3 != 1 {
+			forecastDate.addTimeInterval(-3600)
+		}
+		// get clean date without minutes and seconds
+		let forecastDateComponents = calendar.dateComponents([.year, .month, .day, .hour], from: forecastDate)
+		let cleanForecastDate = calendar.date(from: forecastDateComponents)!
+		// formatting the date to string
+		let formatter = DateFormatter()
+		formatter.timeZone = TimeZone.current
+		formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+		return formatter.string(from: cleanForecastDate)
+	}
+}
+
+extension String {
+	func toDate() -> Date? {
+		let formatter = DateFormatter()
+		formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+		return formatter.date(from: self)
+	}
+}
